eea3145b59887c7db664ba8d5fdf4b14b42cb624
Nithish <nithishattur63@gmail.com>
2025-12-24 22:53:09 +0530

    Create ProductImage.java

---
+++ src/main/java/com/ecom/trends/model/ProductImage.java
@@ -0,0 +1,23 @@
+package com.ecom.trends.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product_images")
+@Getter
+@Setter
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url", length = 500, nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
