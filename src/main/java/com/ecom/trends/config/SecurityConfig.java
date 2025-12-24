e8d10b7b3c83bf433cd3696c4c2d2d2b9876a0fc
Nithish <nithishattur63@gmail.com>
2025-12-24 19:23:32 +0530

    Fix CORS configuration to use frontend.url

--- src/main/java/com/ecom/trends/config/SecurityConfig.java
+++ src/main/java/com/ecom/trends/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
 
         var config = new org.springframework.web.cors.CorsConfiguration();
-        config.setAllowedOrigins(List.of(${frontEnd_url}));  // ALLOW ALL
+        config.addAllowedOrigin(${frontend.url});  // ALLOW ALL
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);     // REQUIRED
