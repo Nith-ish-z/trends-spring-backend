7ff5f4297d9f752b479dc441a5d29cdf4221fcfc
Nithish <nithishattur63@gmail.com>
2025-12-25 14:10:58 +0530

    Update WishItemDTO.java

--- src/main/java/com/ecom/trends/dto/user/WishItemDTO.java
+++ src/main/java/com/ecom/trends/dto/user/WishItemDTO.java
@@ -11,5 +11,5 @@ public class WishItemDTO {
     public Long categoryId;
     public String subCategory;
     public Long subCategoryId;
-    public List<ProductImage> url;
+    public List<String> url;
 }
